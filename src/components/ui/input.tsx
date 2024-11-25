482d139ecb15e5c1f1564859d8443711b82e89de
Ibrahim Saberi <georgeipsum@gmail.com>
2024-11-24 23:56:41 -0500

    feat: update input component to allow icons
    Signed-off-by: Ibrahim Saberi <georgeipsum@gmail.com>

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,20 +3,39 @@ import * as React from "react";
 import { cn } from "~/lib/utils";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  IconLeft?: React.FC;
+  IconRight?: React.FC;
+  containerClassName?: string;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  (
+    { className, type, containerClassName, IconLeft, IconRight, ...props },
+    ref,
+  ) => {
     return (
-      <input
-        type={type}
+      <div
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className,
+          "flex h-10 w-full items-center rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background has-[input:disabled]:cursor-not-allowed has-[input:disabled]:opacity-50 has-[input:focus-visible]:outline-none has-[input:focus-visible]:ring-2 has-[input:focus-visible]:ring-ring has-[input:focus-visible]:ring-offset-2",
+          type === "hidden" && "hidden",
+          (!!IconLeft || !!IconRight) && "gap-2",
+          containerClassName,
         )}
-        ref={ref}
-        {...props}
-      />
+      >
+        {!!IconLeft && <IconLeft />}
+        <input
+          type={type}
+          className={cn(
+            "box-border flex h-full w-full bg-background outline-none file:mr-2 file:cursor-pointer file:rounded file:border-0 file:bg-secondary file:text-xs file:font-medium file:text-foreground placeholder:text-muted-foreground focus:outline-none",
+            type === "file" && "cursor-pointer",
+            className,
+          )}
+          ref={ref}
+          {...props}
+        />
+        {!!IconRight && <IconRight />}
+      </div>
     );
   },
 );
