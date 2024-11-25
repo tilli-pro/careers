4330b89751ffd18409818d39f49561aba418635d
Ibrahim Saberi <georgeipsum@gmail.com>
2024-11-24 23:58:40 -0500

    feat: ~70% of the way to an mvp of application submission
    Signed-off-by: Ibrahim Saberi <georgeipsum@gmail.com>

--- src/features/job-posting/submit.ts
+++ src/features/job-posting/submit.ts
@@ -1,54 +1,110 @@
 "use server";
 
-import { redirect } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
-import type { JobApplication, Prisma } from "@prisma/client";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import type { Prisma } from "@prisma/client";
+import { create } from "domain";
+
+import { db } from "~/server/db";
+
+// TODO: setup env for this
+const client = new S3Client({
+  region: "us-west-2",
+  credentials: {
+    accessKeyId: "",
+    accountId: "",
+    secretAccessKey: "",
+    sessionToken: "",
+  },
+});
 
 export const submitJobApp = async (data: FormData) => {
-  console.log(data);
+  const jobId = data.get("post_id") as string;
+  const slug = data.get("post_slug") as string;
+  if (!slug || !jobId) {
+    return notFound();
+  }
+  data.delete("post_id");
+  data.delete("post_slug");
+
+  const email = data.get("email") as string;
+  if (!email) {
+    return redirect(`/roles/${slug}?failed=email`);
+  }
 
   const application: Partial<Prisma.JobApplicationCreateWithoutApplicantInput> =
     {};
-  const jobId = data.get("post_id") as string;
-  const slug = data.get("post_slug") as string;
+  let applicant = await db.applicant.findFirst({
+    where: { user: { email } },
+  });
+  if (!applicant) {
+    applicant = await db.applicant.create({
+      data: {
+        user: {
+          create: {
+            email,
+          },
+        },
+      },
+    });
+  }
+  application.email = email as string;
+  data.delete("email");
+
+  const name = data.get("name") as string;
+  application.legalName = name;
+  application.preferredName = name;
+  data.delete("name");
+
+  const attribution = data.get("attribution") as string;
+  application.attribution = attribution;
+  data.delete("attribution");
+
+  const resume = data.get("resume") as File;
+  // handle uploading file and setting url
+  if (resume) {
+    const command = new PutObjectCommand({
+      Body: Buffer.from(await resume.arrayBuffer()),
+      ContentType: resume.type,
+      ContentLength: resume.size,
+      Bucket: "",
+      Key: resume.name,
+      Tagging: "",
+    });
+    const upload = await client.send(command);
+    application.resumeUrl = resume.name;
+  } else {
+    return redirect(`/roles/${slug}?failed=resume`);
+  }
+  data.delete("resume");
 
   for (const [question, answer] of data) {
-    switch (question) {
-      case "post_id":
-        break;
-      case "name":
-        application.legalName = answer as string;
-        application.preferredName = answer as string;
-        break;
-      case "email":
-        application.email = answer as string;
-        break;
-      case "about-tilli":
-        application.attribution = answer as string;
-        break;
-      case "resume":
-        if (answer && answer instanceof File) {
-          const resume = answer;
+    if (/social-/.test(question)) {
+      // handle adding relevant socials
+    } else if (/answerid-/.test(question)) {
+      if (!application.answers) {
+        application.answers = { connect: [] };
+      }
 
-          //
-        }
-        break;
-      default:
-        if (!application.answers) application.answers = { connect: [] };
-        if (Array.isArray(application.answers.connect)) {
-          application.answers.connect.push({
-            id: undefined,
-            questionId: question,
-            postingId: jobId,
-            applicantId: "",
-            answer: answer as string,
-          });
-        }
-        break;
+      if (Array.isArray(application.answers.connect)) {
+        application.answers.connect.push({
+          id: undefined,
+          questionId: question,
+          postingId: jobId,
+          applicantId: applicant.id,
+          answer: answer as string,
+        });
+      }
+    } else {
+      console.log({
+        question,
+        answer,
+      });
     }
-
-    console.log(application);
   }
+
+  console.log(application);
 
   return redirect(`/roles/${slug}?submitted`);
 };
