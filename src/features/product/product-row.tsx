fbd6f98e369d4862c4428df3f405241b397a35c9
Michael Vuolo <michaelv@tilli.pro>
2024-12-12 15:06:19 -0500

    fix: adjust font weight and tracking in product row

--- src/features/product/product-row.tsx
+++ src/features/product/product-row.tsx
@@ -58,7 +58,7 @@ const products: ProductItemProps[] = [
         <span
           className={cn(
             useTilliXBaseSerif ? "font-sans" : "font-serif",
-            "relative z-10 text-3xl font-medium drop-shadow-sm",
+            "relative z-10 text-3xl font-medium tracking-wide drop-shadow-sm",
           )}
         >
           tilli
@@ -66,7 +66,7 @@ const products: ProductItemProps[] = [
         <span
           className={
             (cn(useTilliXBaseSerif ? "font-serif" : "font-sans"),
-            "relative z-0 bg-gradient-to-br from-muted via-primary/80 to-primary bg-clip-text text-4xl font-extrabold text-transparent")
+            "relative z-0 bg-gradient-to-br from-muted via-primary/80 to-primary bg-clip-text text-4xl font-bold text-transparent")
           }
         >
           X
