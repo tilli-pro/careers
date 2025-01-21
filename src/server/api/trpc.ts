cfddedbf4338e25e6244343afa190c5dc967cbf3
Ibrahim Saberi <ibrahims@tilli.pro>
2025-01-21 18:18:57 -0500

    refactor(lint)

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -119,14 +119,14 @@ export const publicProcedure = t.procedure.use(timingMiddleware);
  */
 export const protectedProcedure = t.procedure
   .use(timingMiddleware)
-  .use(({ ctx, next }) => {
+  .use(({ ctx, path, next }) => {
     if (!ctx.session || !ctx.session.user) {
       throw new TRPCError({ code: "UNAUTHORIZED" });
     }
     return next({
       ctx: {
         // infers the `session` as non-nullable
-        session: { ...ctx.session, user: ctx.session.user },
+        session: { ...ctx.session, user: ctx.session.user, path },
       },
     });
   });
